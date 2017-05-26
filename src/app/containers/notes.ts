98ded984fc6513e817e97425628fd57161ef1bae
dkni <3539729@gmail.com>
2017-05-26 16:52:54 +0300

    added some functionality

---
+++ src/app/containers/notes.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'notes-container',
+	styles: [`.notes {
+			  padding-top: 50px;
+			}
+			.creator {
+			  margin-bottom: 40px; 
+			}`],
+	template: `<div class="row center-xs notes">
+			  <div class="col-xs-6 creator">
+				note creator here
+			  </div>
+			  <div class="notes col-xs-8">
+				<div class="row between-xs">
+				  <note-card
+					class="col-xs-4"
+					[note]="note"
+					*ngFor="let note of notes; let i = index"
+					(checked)="onNoteChecked(i)"
+				  >
+				  </note-card>
+				</div>
+			  </div>
+			</div>`
+})
+export class NotesContainer {
+	notes = [
+		{ title: 'Eat', value: 'eat some foo', color: 'brown'},
+		{ title: 'Fuck', value: 'bang some wee', color: 'orange'},
+		{ title: 'Die', value: 'drop some dee', color: 'red'}
+	];
+	
+	onNoteChecked(i: number){
+		this.notes.splice(i, 1);
+	}
+};
